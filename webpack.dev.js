8d0952557415ba0ffef8b6d520c9d0165c7b21bc
36t <saluta@gmail.com>
2020-12-16 23:28:56 +0900

    feat: excuted code format.

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,14 +4,14 @@ const commonConfig = require('./webpack.common.js');
 const path = require('path');
 
 module.exports = merge(commonConfig, {
-    mode: 'development',
-    // ソースマップの生成
-    devtool: 'eval-cheap-source-map',
-    // webpack dev serverの設定
-    devServer: {
-       // サーバー起動時に自動的に開く
-        open: true,
-        port: 9000,
-        contentBase: path.resolve(__dirname, 'public')
-    }
+  mode: 'development',
+  // ソースマップの生成
+  devtool: 'eval-cheap-source-map',
+  // webpack dev serverの設定
+  devServer: {
+    // サーバー起動時に自動的に開く
+    open: true,
+    port: 9000,
+    contentBase: path.resolve(__dirname, 'public')
+  }
 })
