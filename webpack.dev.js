5251ab1c29f779ef0bdcd34b7cde9e64b416f6a9
36t <saluta@gmail.com>
2020-12-16 23:13:25 +0900

    feat: added dev server setting.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,17 @@
 // 開発用webpack設定ファイル
 const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
+const path = require('path');
 
 module.exports = merge(commonConfig, {
     mode: 'development',
-    devtool: 'eval-cheap-source-map', // ソースマップの生成
+    // ソースマップの生成
+    devtool: 'eval-cheap-source-map',
+    // webpack dev serverの設定
+    devServer: {
+       // サーバー起動時に自動的に開く
+        open: true,
+        port: 9000,
+        contentBase: path.resolve(__dirname, 'public')
+    }
 })
