4cd09f4e9e0e557c8e8ef70612721dcb5a50839f
36t <saluta@gmail.com>
2020-12-16 22:23:18 +0900

    feat: added false of extra comment / true of drop console.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,20 @@
 // 本番用webpack設定ファイル
 const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(commonConfig, {
-    mode: 'production'
+    mode: 'production',
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false, // ライブラリのライセンスコメントの抽出を防ぐ
+                terserOptions: {
+                    compress: {
+                        drop_console: true // console.logの削除
+                    }
+                }
+            })
+        ]
+    }
 })
