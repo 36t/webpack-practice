365a5bb1c111472b89c817fe1ecd8bab8bd4bbf2
36t <saluta@gmail.com>
2020-12-16 23:30:59 +0900

    mod: updated sample code.

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 import $ from 'jquery'
 import velocity from 'velocity-animate'
 
-console.log('36t')
+console.log('load app.js')
 
 // $('body').append(result)
 velocity($('h1'), 'fadeIn', { duration: 2000, loop: true })
