a01d0e35bcc80a0e56d3e0db6235a93c9507261e
36t <saluta@gmail.com>
2020-12-16 22:36:33 +0900

    feat: import jqeury and velocity-animate and sample logic.

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,9 @@
-console.log('TEST');
+import $ from 'jquery'
+import velocity from 'velocity-animate'
+
+console.log('app')
+
+const result = add(1, 2)
+
+$('body').append(result)
+velocity($('h1'), 'fadeIn', { duration: 2000, loop: true })
