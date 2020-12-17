64c339a35553d87a6cf7823bd366d0ae6c5d7dbe
36t <saluta@gmail.com>
2020-12-17 21:56:34 +0900

    mod: fixed output  and cacheGroups settings.

--- webpack.common.js
+++ webpack.common.js
@@ -12,8 +12,14 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'public'), // 絶対パス必要
-    chunkFilename: '[name].js', // splitChunksの設定はこちらが優先される
-    filename: 'js/[name].[contenthash].js', // 出力するファイルの名前([name]にentryポイントが入る。「.bundle.」ｊは無くても良い)
+    /* 以下2つを有効にすると、全て「bundle」がついてしまう
+    filename: 'js/[name].bundle.js', // 出力するファイルの名前([name]にentryポイントが入る。「.bundle.」は無くても良い)
+    chunkFilename: 'js/[name].js', // splitChunksの設定はこちらが優先される
+    */
+    filename: (pathData) => {
+      // pathData.chunk.name：nameが入る。ここでは「app, another, vendor, vendor-modules」
+      return pathData.chunk.name.search( /vendor/ ) > -1 ? 'js/[name].js': 'js/[name].[contenthash].js';
+    },
   },
   plugins: [
     // 上記outputで指定したディレクトリ以下をクリーンアップ
@@ -30,20 +36,20 @@ module.exports = {
       chunks: ['another'] // 読み込ませたいエントリポイント名を指定する
     })
   ],
-  // ファイルの分割で指定。section5
+  // ファイルの分割で指定
   optimization: {
     splitChunks: {
       chunks: 'initial',  // initial:静的にインポートしているモジュールが対象(要するに「import」の部分が分割の対象になる),
       cacheGroups: {
         // jqueryとvelocity-animateの出力設定
-        vendor: { // 「vendor」の名前は任意で良い。わかりやすい名前を
-          test: /node_modules/, // jqueryとvelocity-animateはnode_modulesの下にあるのでこのように設定
+        vendor1: { // 「vendor」の名前は任意で良い。わかりやすい名前を
+          test: /[\\/]node_modules[\\/]/, // jqueryとvelocity-animateはnode_modulesの下にあるのでこのように設定
           name: 'vendor' // 分割して出力するファイルの名前
         },
         // src/modules/greet.jsの出力設定
         vendorsModules: { // プロパティ名は任意で良い
-          test: /src[\\\\/]js[\\\\/]modules/, // [\\\\/] <= 途中のスラッシュを書く場合 (普通に書くと、Windowsは動作しない)
-          name: 'vendor-modules', // 分割して出力するファイルの名前
+          test: /[\\/]src[\\/]js[\\/]modules[\\/]/, // [\\\\/] <= 途中のスラッシュを書く場合 (普通に書くと、ウインドウズは動作しない)
+           name: 'vendor-modules', // 分割して出力するファイルの名前
           minSize: 0, // 分割の対象となるモジュールの最小サイズ。デフォルトは30kb。今回めっちゃ小さいので
           minChunks: 2 // モジュールがいくつの場所で利用されていれば分割の対象とするか
         }
