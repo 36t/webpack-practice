8fabb592a5af31725a7523a8c77448c6cb50a720
36t <saluta@gmail.com>
2020-12-17 22:48:42 +0900

    feat: added polyfill setting.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-    presets: ['@babel/preset-env']
+  presets: [
+    [
+      '@babel/preset-env',
+      // ポリフィルの設定を追加 (babel/preset-envのオプションを設定)
+      {
+        useBuiltIns: 'usage', // usageにすることで必要なポリフィルのみを取り込んでいる
+        corejs: 3, // corejsのバージョン。設定しない場合２でエラーがでる
+        debug: true // 取り込まれたポリフィルの情報が出力、確認用のオプションなので、不要の場合は削除しても良い
+      }
+    ]
+  ]
 }
